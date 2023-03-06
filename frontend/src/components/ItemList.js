51f50a94870e9f6ae2eb9670b5437cc9c6cf345b
psyga2 <psyga2@exmail.nottingham.ac.uk>
2023-03-06 20:00:15 +0000

    Format fixes.

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -9,10 +9,10 @@ const ItemList = (props) => {
 
   if (props.noResults) {
     return (
-      <div className ="py-4 no-items2" id ="empty">
+      <div className="py-4 no-items2" id="empty">
         No Items were found for your search
       </div>
-    )
+    );
   }
   if (props.items.length === 0) {
     return <div className="py-4 no-items">No items are here... yet.</div>;
