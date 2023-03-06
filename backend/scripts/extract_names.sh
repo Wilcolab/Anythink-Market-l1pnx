f68650a10e5fb90eb77e5d0566146805cc1750c4
Gavin Almey <95375237+Thndrrrrrr@users.noreply.github.com>
2023-03-06 22:06:03 +0000

    test2

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-grep -i "@amazon.com" MOCK_DATA.csv | cut -d , -f 2-3 | awk -F ',' '{print $2 " " $1}' > output_names.txt
+grep -i "@amazon.com" $1 | cut -d , -f 2-3 | awk -F ',' '{print $2 " " $1}' > output_names.txt
