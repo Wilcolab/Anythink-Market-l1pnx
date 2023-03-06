bde256d7041b4af1a1fe8e2b66e37cf333fa4e93
psyga2 <psyga2@exmail.nottingham.ac.uk>
2023-03-06 20:42:12 +0000

    Added search visibility.

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "../../imgs/logo.png";
 import agent from "../../agent";
+import { useState } from "react";
 
 const Banner = (props) => {
+  const [searchVisible, setSearchVisible] = useState(false);
   const onSearchChange = (event) => {
     props.onSearchFilter(
       event.target.value,
@@ -15,13 +17,23 @@ const Banner = (props) => {
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
+          <span>A place to</span>
+          <span
+            id="get-part"
+            onClick={() => {
+              setSearchVisible(true);
+            }}
+          >
+            get
+          </span>
+          {searchVisible && (
           <input
             type="text"
             id="search-box"
             placeholder="What is it that you truly desire?"
             onChange={onSearchChange}
           ></input>
+          )}
           <span> the cool stuff.</span>
         </div>
       </div>
