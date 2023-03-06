03ab19952db9eac9ad389f7c488815066c578d0d
psyga2 <psyga2@exmail.nottingham.ac.uk>
2023-03-06 19:59:11 +0000

    Created a message if invalid search is performed.

--- frontend/src/reducers/itemList.js
+++ frontend/src/reducers/itemList.js
@@ -46,6 +46,17 @@ const reducer = (state = {}, action) => {
         tag: action.tag,
         currentPage: 0,
       };
+      case APPLY_TITLE_FILTER:
+        return {
+          ...state,
+          pager: action.pager,
+          items: action.payload.items,
+          itemsCount: action.payload.itemsCount,
+          tab: null,
+          tag: action.title,
+          currentPage: 0,
+          noResults: action.payload.items.length === 0
+        };
     case HOME_PAGE_LOADED:
       return {
         ...state,
